a14cd4da0b520c0ac127fad61e315817ad144c32
Hakan Hidir <hakan.hidir@technicsatelier.com>
2022-09-29 02:29:33 +0200

    initial commit

---
+++ tooglefeature-api/Controllers/NewsController.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.FeatureManagement;
+using Microsoft.FeatureManagement.Mvc;
+
+namespace tooglefeature_api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+[FeatureGate("NewsAPI")]
+public class NewsController : ControllerBase
+{
+    private readonly ILogger<NewsController> _logger;
+    private readonly IFeatureManager _featureManager;
+
+    public NewsController(ILogger<NewsController> logger, IFeatureManager featureManager)
+    {
+        _logger = logger;
+        _featureManager = featureManager;
+    }
+
+    [FeatureGate("SportAPI")]
+    [HttpGet]
+    [Route("[action]")]
+    public IActionResult SportNews()
+    {
+        return Ok(new string[] { "SportNews 1", "SportNews 2", "SportNews 3" });
+
+    }
+
+    [FeatureGate("WeatherAPI")]
+    [HttpGet]
+    [Route("[action]")]
+    public IActionResult WeatherNews()
+    {
+        return Ok(new string[] { "Weather News Berlin", "Weather News Stuttgart", "Weather News Esslingen" });
+
+    }
+
+    //[FeatureGate("EconomyAPI")]
+    [HttpGet]
+    [Route("[action]")]
+    public async Task<IActionResult> EconomyNews()
+    {
+
+        if (await _featureManager.IsEnabledAsync("EconomyAPI"))
+        {
+            return Ok(new string[] { "Money News", "Crypto News", "Gold News" });
+        }
+
+        return BadRequest();
+    }
+
+    [FeatureGate("MagazineAPI")]
+    [HttpGet]
+    [Route("[action]")]
+    public IActionResult MagazineNews()
+    {
+        return Ok(new string[] { "BradPitt News", "Angelina Jolie News" });
+
+    }
+
+    [FeatureGate("WeekendNewsAPI")]
+    [HttpGet]
+    [Route("[action]")]
+    public IActionResult WeekendNews()
+    {
+        return Ok(new string[] { "Weekend Special News" });
+
+    }
+}
+
---
+++ tooglefeature-api/Program.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.FeatureManagement;
+using Microsoft.FeatureManagement.FeatureFilters;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+#region FeatureFlagManagement
+
+builder.Services.Configure<FeatureManagementOptions>(options =>
+{
+    options.IgnoreMissingFeatureFilters = true;
+});
+
+builder.Services.AddFeatureManagement(builder.Configuration.GetSection("MyFeatures"))
+    .AddFeatureFilter<PercentageFilter>()
+    .AddFeatureFilter<TimeWindowFilter>();
+
+//Also, You can use FeatureManagement with
+//-Middlewares
+//-HttpContext
+//-MvcViews
+//-Razor Pages
+//-MvcFilters
+//-etc.
+
+#endregion
+
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
